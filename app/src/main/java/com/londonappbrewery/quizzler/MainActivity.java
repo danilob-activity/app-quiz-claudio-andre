d538f7b13f8e182eb1141336eb1acdf4754af2d5
Cláudio André <andretrafalgar@gmail.com>
2019-05-23 11:53:24 -0300

    2ºcommit

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.londonappbrewery.quizzler.models.TrueFalse;
+
 public class MainActivity extends Activity {
 
     // TODO: Declare constants here
@@ -20,23 +22,31 @@ public class MainActivity extends Activity {
     TextView mQuestionTextView;
     ProgressBar mProgressBar;
 
+    int mIndex;
+    int mScore;
+    int mQuestion;
+
+
 
     // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
+
+    final int PROGRESS_BAR_INCREMENT = (int) Math.ceil(100.0 /
+            mQuestionBank.length);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +59,20 @@ public class MainActivity extends Activity {
         mQuestionTextView = findViewById(R.id.question_text_view);
         mProgressBar = findViewById(R.id.progress_bar);
 
+        //valores iniciais
+        mIndex = 0;
+        mScore = 0;
+        mQuestion = mQuestionBank[mIndex].getQuestion();
+        mQuestionTextView.setText(mQuestion);
+        mScoreTextView.setText("Score: "+mScore+"/"+mQuestionBank.length);
+
+
+
+
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_SHORT).show();
                 checkAnswer(true);
                 updateQuestion();
@@ -61,7 +81,7 @@ public class MainActivity extends Activity {
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_SHORT).show();
                 checkAnswer(false);
                 updateQuestion();
@@ -70,30 +90,20 @@ public class MainActivity extends Activity {
 
     }
 
-       /* public class TrueFalse{
-            private int mQuestion;
-            private boolean mAswer;
 
-            public TrueFalse(int questionId, int trueOrFalse){
-                return;
-            }
-
-            public int getQuestion() {
-                return 0;
-            }
-
-            public boolean mAswer(){
-                return mAswer;
-            };
-
-
-        }*/
-
-    private boolean checkAnswer(boolean b) {
-        return b;
+    private void checkAnswer(boolean answer) {
+        if (answer==mQuestionBank[mIndex].mAnswer()){
+            mScore++;
+            Toast.makeText(getApplicationContext(),
+                    R.string.correct_toast, Toast.LENGTH_LONG).show();
+        }
+            Toast.makeText(getApplicationContext(),
+                R.string.incorrect_toast, Toast.LENGTH_LONG).show();
+        mIndex++;
     }
 
     private void updateQuestion() {
+        mProgressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
 
     }
 }
---
+++ app/src/main/java/com/londonappbrewery/quizzler/models/TrueFalse.java
@@ -0,0 +1,23 @@
+package com.londonappbrewery.quizzler.models;
+
+/**
+ * Created by aluno on 23/05/19.
+ */
+
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mAnswer;
+
+    public TrueFalse(int question, boolean aswer) {
+        mQuestion = question;
+        mAnswer = aswer;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public boolean mAnswer() {
+        return false;
+    }
+}
